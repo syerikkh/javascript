1d289bd67812e22e7609947fbe521ff5ad4493c4
syerikkh <147494171+syerikkh@users.noreply.github.com>
2023-11-21 14:43:54 +0800

    home
    11.21

---
+++ To-do List/todo.js
@@ -0,0 +1,11 @@
+
+
+function addTask() {
+    const taskInput = document.getElementById("inputTask");
+    const taskList = document.getElementById("taskList");
+
+    if (taskInput.value !== '') {
+
+    }
+
+}
