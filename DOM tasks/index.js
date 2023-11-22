322453d214fbad26eac08305bf95b9d0e937e66a
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2023-11-22 19:15:42 +0800

    11.22

--- DOM tasks/index.js
+++ DOM tasks/index.js
@@ -1,1 +1,73 @@
-document.getElementById("haha").textContent();
+// const text = document.getElementById("haha").textContent = "hoho";
+
+
+// const style = document.querySelector("#haha").style.color = "red";
+
+
+// function changeTxt() {
+//     document.querySelector("#haha").textContent = "hoho";
+// }
+
+// const changeBtn = document.getElementById("changeText");
+// changeBtn.addEventListener("click", changeTxt);
+
+
+// const newEl = document.createElement("div");
+// newEl.innerText = "kkkk";
+// document.getElementById("haha").appendChild(newEl);
+
+
+// const toggle = document.getElementById("haha");
+// toggle.style.display = "none";
+
+// const button = document.getElementById("changeText");
+// button.addEventListener("click", function () {
+//     toggle.style.display = "block";
+// })
+
+
+// let list;
+// for (i = 0; i < 10; i++) {
+//     list = document.createElement("li");
+//     document.getElementById("allList").appendChild(list);
+// }
+
+
+// let tables;
+// for (i = 1; i <= 10; i++) {
+//     tables = document.createElement("td");
+//     tables.innerText = "hahahahahah"
+//     tables.style.border = "1px solid"
+//     tables.style.display = "flex";
+//     if (i % 2 == 0) {
+//         tables.style.backgroundColor = "orange";
+//     } else { tables.style.backgroundColor = "green" };
+//     document.getElementById("table").appendChild(tables);
+// }
+
+// let option;
+// for (i = 1; i <= 10; i++) {
+//     option = document.createElement("option");
+//     option.textContent = `Car ${i}`
+//     document.getElementById("cars").appendChild(option);
+// }
+
+const messages = ["hahahaha",
+    "heheheheh",
+    "hohohohoh",
+    "hihihi",
+    "huhuh"];
+
+
+
+function delay() {
+    for (let i = 0; i < messages.length; i++) {
+        setTimeout(() => {
+            console.log(messages[i]);
+        }
+            , i * 1000);
+    }
+}
+delay();
+
+
--- chat.js
+++ chat.js
@@ -1,16 +1,21 @@
-function divideDiv(div) {
-    // Create left and right half divs
-    const leftHalf = document.createElement("div");
-    const rightHalf = document.createElement("div");
+// Array of messages to display
+const messages = [
+    "Hello!",
+    "How are you?",
+    "I hope you're doing well.",
+    "Let me know if you have any questions!",
+];
 
-    // Assign classes for styling
-    leftHalf.classList.add("half-div", "left-half");
-    rightHalf.classList.add("half-div", "right-half");
-
-    // Append half divs to the clicked div
-    div.appendChild(leftHalf);
-    div.appendChild(rightHalf);
-
-    // Remove the click event listener to avoid further division
-    div.removeEventListener("click", divideDiv);
+// Function to display messages with a delay
+function displayMessages() {
+    // Iterate through the messages using a for loop
+    for (let i = 0; i < messages.length; i++) {
+        // Use setTimeout to create a delay for each message
+        setTimeout(() => {
+            console.log(messages[i]); // Display the current message
+        }, i * 1000); // The delay is multiplied by i to create a staggered effect
+    }
 }
+
+// Call the function to start displaying messages
+displayMessages();
