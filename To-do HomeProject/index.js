ef9f99d962b2b46c499be6806d35122ee4fe3bb9
syerikkh <147494171+syerikkh@users.noreply.github.com>
2023-11-27 03:08:31 +0800

    home
    11.26

--- To-do HomeProject/index.js
+++ To-do HomeProject/index.js
@@ -9,22 +9,24 @@ addCardButtons.forEach(function (button) {
 let tasks = [];  // Hooson task array
 
 function addTask() {  // Task add function
-    const addTaskBackground = document.querySelector(".addTaskBackground");
-    addTaskBackground.style.display = "none"
     const titleInputRef = document.getElementById("title");
     const descriptionInputRef = document.getElementById("description");
     const priorityRef = document.getElementById("priority");
+    const statusRef = document.getElementById("status");
     const newTask = {
         title: titleInputRef.value,
         description: descriptionInputRef.value,
-        priority: priorityRef.value
+        priority: priorityRef.value,
+        status: statusRef.value
     };
 
-    if (newTask.title !== "" && newTask.description !== "") {
+
+    if (newTask.title !== "" && newTask.description !== "" && newTask.description !== "" && newTask.status !== "") {
         tasks.push(newTask);
-        titleInputRef = ""; // value hoosloh
-        descriptionInputRef = "";
-        priorityRef = "";
+        titleInputRef.value = ""; // value hoosloh
+        descriptionInputRef.value = "";
+        priorityRef.value = "";
+        statusRef.value = "";
         render();
     }
 
@@ -34,12 +36,40 @@ function addTask() {  // Task add function
 document.getElementById("addTaskButton").addEventListener("click", addTask);
 
 function render() {
-    const tasksList = document.getElementById("tasks");
-    tasksList.innerHTML = "";
-    tasks.forEach(task => {
-        const taskElement = document.createElement("li");
-        taskElement.textContent = `Title: ${task.title}, Description: ${task.description}, Priority: ${task.priority}`;
-        tasksList.appendChild(taskElement);
+
+    const addTaskBackground = document.querySelector(".addTaskBackground");
+    addTaskBackground.style.display = "none";
+
+    document.querySelectorAll(".tasks").forEach(tasksList => {
+        tasksList.innerHTML = "";
     });
 
+    tasks.forEach(task => {
+        const tasksList = getTasksList(task.status);
+        const addedCard = document.querySelector(".addedCard");
+        if (tasksList) {
+            const doneButton = document.createElement("button");
+            doneButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+            const taskElement = document.createElement("li");
+            taskElement.innerHTML = `<strong>${task.title}</strong><br>${task.description}<br><span style="border: 1px solid grey;">${task.priority}</span>`;
+            tasksList.appendChild(taskElement);
+            addedCard.appendChild(doneButton);
+            addedCard.appendChild(tasksList);
+        }
+    });
 }
+
+function getTasksList(status) {
+    if (status === "to-do") {
+        return document.getElementById("to-do").querySelector(".tasks");
+    } else if (status === "in-progress") {
+        return document.getElementById("in-progress").querySelector(".tasks");
+    } else if (status === "stuck") {
+        return document.getElementById("stuck").querySelector(".tasks");
+    } else if (status === "done") {
+        return document.getElementById("done").querySelector(".tasks");
+    } else {
+        return null;
+    }
+};
+
