95a2ea114182c588a6b9d63e93eddd7ed19b6505
94241021 Khuanysh <23LP2243@G304-CC0080.local>
2023-11-27 19:31:08 +0800

    11.27

--- To-do HomeProject/index.js
+++ To-do HomeProject/index.js
@@ -13,11 +13,14 @@ function addTask() {  // Task add function
     const descriptionInputRef = document.getElementById("description");
     const priorityRef = document.getElementById("priority");
     const statusRef = document.getElementById("status");
+    var id = "id" + Math.random().toString(16).slice(2)
+
     const newTask = {
         title: titleInputRef.value,
         description: descriptionInputRef.value,
         priority: priorityRef.value,
-        status: statusRef.value
+        status: statusRef.value,
+        id: id
     };
 
 
@@ -33,7 +36,17 @@ function addTask() {  // Task add function
 
 };
 
-document.getElementById("addTaskButton").addEventListener("click", addTask);
+const addTaskButton = document.getElementById("addTaskButton");
+addTaskButton.addEventListener("click", addTask);
+
+document.querySelector(".addTaskCard").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
+
+
+
 
 function render() {
 
@@ -42,20 +55,58 @@ function render() {
 
     document.querySelectorAll(".tasks").forEach(tasksList => {
         tasksList.innerHTML = "";
+
     });
 
+
     tasks.forEach(task => {
+
         const tasksList = getTasksList(task.status);
-
         if (tasksList) {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            card.draggable = "true";
+
+            const doneButton = document.createElement("button");
+            const removeButton = document.createElement("button");
+            removeButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+            removeButton.classList.add("removeButton");
+            removeButton.id = task.id;
+
+            removeButton.onclick = function () {
+                tasks = tasks.filter(task => task.id !== removeButton.id);
+                render()
+            };
+            doneButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+            doneButton.classList.add("doneButton");
+            doneButton.onclick = function () {
+                document.getElementById("done").querySelector(".tasks").appendChild(tasksList);
+            }
             const taskElement = document.createElement("li");
-            taskElement.innerHTML = `<strong>${task.title}</strong><br>${task.description}<br><span style="border: 1px solid grey;">${task.priority}</span>`;
-            tasksList.appendChild(taskElement);
 
+            taskElement.classList.add("taskElement");
+            taskElement.innerHTML = `<h3 class="title">${task.title}</h3> ${task.description} <p class= "statusStyle">${task.priority}</p>`;
+            card.appendChild(doneButton);
+            card.appendChild(taskElement);
+            card.appendChild(removeButton);
+            tasksList.appendChild(card);
+        }
+        function dragStart(ev) {
+            ev.dataTransfer.setData("text/plain", ev.target.id);
+            console.log("dragstart");
+        };
+        function dragOver(ev) {
+            ev.preventDefault();
 
+        };
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text/plain");
+            ev.target.appendChild(document.getElementById(data));
 
         }
     });
+
 }
 
 function getTasksList(status) {
@@ -70,5 +121,6 @@ function getTasksList(status) {
     } else {
         return null;
     }
+
 };
 
